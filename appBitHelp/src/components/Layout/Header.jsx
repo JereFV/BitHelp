bce8da6f9dedb9c7309f8672512dc4e63bd32b65
Rodrigo Herrera <120344564+Rodri-exe@users.noreply.github.com>
2025-10-12 20:50:55 -0600

    add test

--- appBitHelp/src/components/Layout/Header.jsx
+++ appBitHelp/src/components/Layout/Header.jsx
@@ -23,7 +23,7 @@ export default function Header() {
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
+            BitHelp
           </Typography>
           <Button color="inherit">Login</Button>
         </Toolbar>
