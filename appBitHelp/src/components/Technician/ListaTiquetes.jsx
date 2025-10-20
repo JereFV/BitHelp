ef0df37c74f8f5fe2e897c08f7300c1b195d456b
jeysonAlfaro <jeysonalfaro83@gmail.com>
2025-10-19 22:02:40 -0600

    Add DetalleTecnicos, ListaTecnicos, ListaTiquetes

---
+++ appBitHelp/src/components/Technician/ListaTiquetes.jsx
@@ -0,0 +1,22 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import { Box, Typography } from '@mui/material'; 
+
+// El componente de tu nueva pantalla
+function ListaTiquetes() {
+  return (
+    // Usa Box para contenedores y sx para estilos de Material UI
+    <Box sx={{ padding: 4, textAlign: 'center' }}>
+      <Typography variant="h3" component="h1" color="primary" gutterBottom>
+        Listado de [Tiquetes]
+      </Typography>
+      <Typography variant="body1">
+        Este contenido se muestra dentro del Layout (con Header y Footer) 
+        gracias al Router. ¡Ya puedes empezar a diseñar!
+      </Typography>
+      {/* ... Añade aquí el resto de tu diseño JSX ... */}
+    </Box>
+  );
+}
+
+export default ListaTiquetes;
