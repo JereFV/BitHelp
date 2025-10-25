140a5f6c3322a920d23aa973bdd333cb34734ec7
herr3r4sofi <shc290108@gmail.com>
2025-10-25 10:06:09 -0600

    add User context and utilities para poder extrar usuario de db (parte del login)

--- appBitHelp/src/components/Home/Home.jsx
+++ appBitHelp/src/components/Home/Home.jsx
@@ -1,7 +1,12 @@
-import React from 'react'; 
+import React, { useContext } from 'react';
 import Container from '@mui/material/Container'; 
 import Typography from '@mui/material/Typography'; 
+import { UserContext } from '../../context/UserContext';
+
 export function Home() { 
+  // Valor por defecto mientras se realiza el login
+  const { getUserName } = useContext(UserContext)
+  const username = getUserName(); // Obtiene el nombre del token decodificado o 'Invitado'.
   return ( 
     <Container sx={{ p: 2 }} maxWidth="sm"> 
       <Typography 
@@ -13,6 +18,8 @@ export function Home() {
         fontFamily={'-apple-system'}
         marginBottom={1}
       > 
+        <span style={{ fontSize: '70%' }}>Bienvenid@ {username}</span>
+        <br />
         BitHelp 
         </Typography> 
       <Typography variant="h5" align="center" color="text.secondary" fontFamily={'-apple-system'}> 
