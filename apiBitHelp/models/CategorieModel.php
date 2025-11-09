fea73a6e6a52e9b4d02450b2ce172e189bd74ff9
jeysonAlfaro <jeysonalfaro83@gmail.com>
2025-11-09 03:38:42 -0600

    Add: tabla categoria_especialidad, datos iniciales y ajustes en métodos de CategorieModel

--- apiBitHelp/models/CategorieModel.php
+++ apiBitHelp/models/CategorieModel.php
@@ -9,64 +9,63 @@ class CategorieModel
     }
 
     public function getAllCategories()
-    {
-        try {
-            //Consulta SQL  con Join para obtener los datos el SLA en base a su ID.
-            $query = "
-            SELECT 
-                c.idCategoria,
-                c.nombre as nombreCategoria,
-                c.estado as estadoCategoria,
-                c.idSla,
-                s.tiempoMaxRespuesta,
-                s.tiempoMaxResolucion,
-                GROUP_CONCAT(DISTINCT e.nombre ORDER BY e.nombre SEPARATOR '|||') AS especialidades_concatenadas,
-                GROUP_CONCAT(DISTINCT et.nombre ORDER BY et.nombre SEPARATOR '|||') AS etiquetas_concatenadas
-            FROM
-                categoria c
-            INNER JOIN 
-                sla s on c.idSla = s.idSla   
-            LEFT JOIN
-                especialidad e ON c.idCategoria = e.idCategoria
-            LEFT JOIN
-                etiqueta_categoria ec ON c.idCategoria = ec.idCategoria
-            LEFT JOIN
-                etiqueta et ON ec.idEtiqueta = et.idEtiqueta
-            GROUP BY
-                c.idCategoria, c.nombre, c.estado, c.idSla, s.tiempoMaxRespuesta, s.tiempoMaxResolucion
-            ORDER BY
-                c.idCategoria;             
-            ";
+{
+    try {
+        $query = "
+        SELECT 
+            c.idCategoria,
+            c.nombre AS nombreCategoria,
+            c.estado AS estadoCategoria,
+            c.idSla,
+            s.tiempoMaxRespuesta,
+            s.tiempoMaxResolucion,
+            GROUP_CONCAT(DISTINCT e.nombre ORDER BY e.nombre SEPARATOR '|||') AS especialidades_concatenadas,
+            GROUP_CONCAT(DISTINCT et.nombre ORDER BY et.nombre SEPARATOR '|||') AS etiquetas_concatenadas
+        FROM
+            categoria c
+        INNER JOIN 
+            sla s ON c.idSla = s.idSla
+        LEFT JOIN
+            categoria_especialidad ce ON c.idCategoria = ce.idCategoria
+        LEFT JOIN
+            especialidad e ON ce.idEspecialidad = e.idEspecialidad
+        LEFT JOIN
+            etiqueta_categoria ec ON c.idCategoria = ec.idCategoria
+        LEFT JOIN
+            etiqueta et ON ec.idEtiqueta = et.idEtiqueta
+        GROUP BY
+            c.idCategoria, c.nombre, c.estado, c.idSla, s.tiempoMaxRespuesta, s.tiempoMaxResolucion
+        ORDER BY
+            c.idCategoria;
+        ";
 
-            //Ejecucción de la consulta.
-            $categorie = $this->connection->ExecuteSQL($query);
-
-            return $categorie;
-        } 
-        catch (Exception $ex)
-        {
-            handleException($ex);
-        }
+        $categorie = $this->connection->ExecuteSQL($query);
+        return $categorie;
+    } 
+    catch (Exception $ex) {
+        handleException($ex);
     }
+}
+
 
     //Obtiene la categoría de un tiquete a partir de la especialidad del mismo.
     public function getBySpecialty($idSpecialty)
-    {
-        try
-        {
-            $query = "SELECT a.idCategoria, a.nombre
-                      FROM  categoria a
-                      INNER JOIN especialidad b 
-                      ON a.idCategoria = b.IdCategoria
-                      WHERE b.idEspecialidad = $idSpecialty";
+{
+    try {
+        $query = "
+        SELECT c.idCategoria, c.nombre
+        FROM categoria c
+        INNER JOIN categoria_especialidad ce ON c.idCategoria = ce.idCategoria
+        INNER JOIN especialidad e ON ce.idEspecialidad = e.idEspecialidad
+        WHERE e.idEspecialidad = $idSpecialty
+        ";
 
-            $categorie = $this->connection->executeSQL($query);
-
-            //Devuelve el nombre y id de la categoria en una estructura.
-            return $categorie;
-        } 
-        catch (Exception $ex) {
-            handleException($ex);
-        }
+        $categorie = $this->connection->executeSQL($query);
+        return $categorie;
+    } 
+    catch (Exception $ex) {
+        handleException($ex);
     }
+}
+
 } 
